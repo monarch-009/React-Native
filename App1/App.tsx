c66533ca8f252e2ce82df71f3f0b3e2b62a3175f
Aditya Singh <116072555+monarch-009@users.noreply.github.com>
2024-09-30 12:51:04 +0530

    Add files via upload

---
+++ App1/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Monarch Word</Text>
+        <Text>Monarch Word</Text>
+        <Text>Monarch Word</Text>
+        <Text>This is my First app which i had made with the help of react native</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
